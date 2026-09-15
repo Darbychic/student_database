ae629155f45a0f85e1f5aa5b210021a0b056faf0
Darbychic <darbychic26@gmail.com>
2026-09-15 18:15:18 +0100

    added login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from'express';
 const userRouter = express.Router();
-import { createUser, updateUser, deleteUser, getUser} from '../controller/userController.js';
+import { createUser, loginUser, updateUser, deleteUser, getUser, } from '../controller/userController.js';
 
 userRouter.post('/:new-users', createUser);
+userRouter.get('/login', loginUser)
 userRouter.put('/:student_regNo', updateUser);
 userRouter.delete('/:student_regNo', deleteUser);
 userRouter.get('/:id', getUser);
